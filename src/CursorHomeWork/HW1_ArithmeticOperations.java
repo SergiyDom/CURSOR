911416987dee90c36d25fae3b396f17a0ca62c5b
Sergiy D <SergiyDom@users.noreply.github.com>
2020-06-19 11:26:37 +0300

    first commit

---
+++ src/CursorHomeWork/HW1_ArithmeticOperations.java
@@ -0,0 +1,28 @@
+package CursorHomeWork;
+
+public class HW1_ArithmeticOperations {
+    public static void main(String[] args) {
+
+        byte b = 8;
+        short s = 16;
+        long l = 300;
+        double d = 28.8;
+        float f = 288.8f;
+
+        System.out.println("b + s = " + (b + s));
+        System.out.println("l + d = " + (l + d));
+        System.out.println("d + f = " + (d + f));
+
+        System.out.println("b - s = " + (b - s));
+        System.out.println("l - d = " + (l - d));
+        System.out.println("d - f = " + (d - f));
+
+        System.out.println("b * s = " + b * s);
+        System.out.println("l * d = " + l * d);
+        System.out.println("d * f = " + d * f);
+
+        System.out.println("b / s = " + (b*1.0) / s);
+        System.out.println("l / d = " + l / d);
+        System.out.println("d / f = " + d / f);
+    }
+}
